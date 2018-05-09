6e197bf2a21e1c1ff5233ae29f58450553cbe3f5
AmberFazal <Amberfazal1@gmail.com>
2018-05-09 22:34:08 +0500

    create customer controller

---
+++ project/project/Models/Mydb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace project.Models
+{
+    public class Mydb : DbContext
+    {
+        public Mydb() : base("mycon")
+        { }
+        public DbSet<customer> customer { get; set; }
+       
+
+    }
+}
