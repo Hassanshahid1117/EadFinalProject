37dbbcc7f5b11d718b417be29ae8a502f1589c92
AmberFazal <Amberfazal1@gmail.com>
2018-05-12 23:17:01 +0500

    Cart Controller and view

--- project/project/Models/Mydb.cs
+++ project/project/Models/Mydb.cs
@@ -11,7 +11,9 @@ namespace project.Models
         public Mydb() : base("mycon")
         { }
         public DbSet<customer> customer { get; set; }
-        public DbSet<Products> products { get; set;;}
+        public DbSet<Products> products { get; set;}
+        public DbSet<cart> cart { get; set; }
+
 
 
     }
